53881d1d6b8f4d6bdc6aa615dfb5bd264a5790b7
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-05-28 00:44:07 -0400

    displayed icon and temp

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,7 +1,5 @@
 var apiKey = "c6853d2002a12eaec2efc01b665b157c";
 
-// current weather: city name, date, an icon for weathes conditions, temperature, humidity, wind speed, and UV index
-
 $(document).ready(function () {
 
     //when page loads display all user city inputs
@@ -25,17 +23,40 @@ $(document).ready(function () {
 
 });
 
-//global city variable
-
 var getWeather = function(city) {
 
     // format the github api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        });
+    var info = {
+        "async": true,
+        "crossDomain": true,
+        "url": apiUrl,
+        "method": "GET"
+    }
+
+    $.ajax(info).done(function (response) {
+    console.log(response);
+
+    // display icon for current weather condition 
+    var url = "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
+    $("#temp").append($('<img>',{id:'temp',src:url}));
+
+    // display temperature
+    var temp = response.main.temp
+    $("#temp").append("Temp:" + temp + "°F");
     });
+
+    // display city name 
+    
+    // display current date
+
+    // display humidity 
+
+    // display wind speed
+
+    // display uv index 
+
+        // add color for favorable, moderate or severe conditions
+
 };
